d0d0341671e2839788032a157fa67d2cbf8e7d70
ivanpoveda <ivanpoveda0206@gmail.com>
2025-11-22 19:31:44 -0600

    Agregar archivos de proyecto.

---
+++ LaboratoriosCampusTech/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LaboratoriosCampusTech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
