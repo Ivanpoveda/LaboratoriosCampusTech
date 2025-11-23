35e481c72680ff1c66ca781b9baa93571b0ae1db
ivanpoveda <ivanpoveda0206@gmail.com>
2025-11-22 22:08:34 -0600

    Se realiza la creacion en la carpeta Controllers, el archivo ReservationsController.cs y adjuntra el codigo

---
+++ LaboratoriosCampusTech/Controllers/ReservationsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LaboratoriosCampusTech.Data;
+using LaboratoriosCampusTech.Models;
+
+namespace LaboratoriosCampusTech.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        private readonly IReservationRepository _repo;
+
+        public ReservationsController(IReservationRepository repo)
+        {
+            _repo = repo;
+        }
+        public IActionResult Index()
+        {
+            return View(_repo.GetAll());
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Reservation { ReservationDate = DateTime.Today });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Reservation reservation)
+        {
+            if (_repo.ExistsByCode(reservation.ReservationCode))
+                ModelState.AddModelError(nameof(reservation.ReservationCode), "Código ya existe.");
+
+            if (!ModelState.IsValid)
+                return View(reservation);
+
+            _repo.Add(reservation);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
